8e1f20f3203e09ead4757060b2c151a5a04331c8
scarlett.kim <scarlett.kim@kakaocorp.com>
2018-07-04 23:17:19 +0900

    참고 링크 깃 위키에 옮겨둠

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,3 @@
 import '../css/test.css';
 //require('./이미지경로/이미지파일명');
 import './test';
-// 해당 링크들 정리하기
-
-// css loader, file loader 셋팅 
-// https://www.zerocho.com/category/Webpack/post/58ac2d6f2e437800181c1657 
-// webpack 공홈
-// https://webpack.js.org/
-/*
-
-셋팅 참고
-https://itnext.io/how-to-write-a-frontend-javascript-plugin-using-es6-sass-webpack-a1c6d6fdeb71
-https://hackernoon.com/how-to-build-a-react-project-from-scratch-using-webpack-4-and-babel-56d4a26afd32
-https://medium.freecodecamp.org/a-beginners-introduction-to-webpack-2620415e46b3
-https://medium.freecodecamp.org/learn-webpack-for-react-a36d4cac5060
-https://jaeyeophan.github.io/2017/05/05/webpack-tutorial-1/
-
-https://github.com/FEDevelopers/tech.description/wiki/Webpack%EC%9D%98-%ED%98%BC%EB%9E%80%EC%8A%A4%EB%9F%B0-%EC%82%AC%ED%95%AD%EB%93%A4
-
-
-postCSs
-https://postcss.org/
-cssnano
-https://cssnano.co/
-autoprefixer
-https://github.com/postcss/autoprefixer
-
-devServer
-https://webpack.js.org/configuration/dev-server/
-
-webpackconfig
-https://firejune.com/1798/%EC%B4%88%EB%B3%B4%EC%9E%90%EC%9A%A9+Webpack+%ED%8A%9C%ED%86%A0%EB%A6%AC%EC%96%BC+%ED%8C%8C%ED%8A%B81+-+Webpack+%EC%9E%85%EB%AC%B8
-
-ExtractTextPlugin minimize
-https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/434
-
-*/
