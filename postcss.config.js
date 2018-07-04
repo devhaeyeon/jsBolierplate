30deb3facff52ed4dc88959503e91896df02e327
scarlett.kim <scarlett.kim@kakaocorp.com>
2018-07-04 23:11:22 +0900

    javascript dev env bolierplate

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+const config={
+    plugins: [
+        require('autoprefixer'),
+        require('cssnano'),
+    ]
+};
+module.exports = config;
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const uglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config={
+    entry : './src/js/index.js',
+    devServer : {
+        contentBase:path.join(__dirname,'dist'),
+        compress:true,
+        port:7777
+    },
+    output : {
+        path:path.join(__dirname,'/dist'),
+        filename:'index.js'
+    },
+    module : {
+        rules :[
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                use:['babel-loader']
+            },
+            {
+                test:/\.png/,
+                use:{
+                    loader:'url-loader',
+                    options:{
+                        name:'[name].[ext]?[hash]',
+                        publicPath:'./dist/'
+                    }
+                }
+            },
+            {
+                test:/\.css$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:'style-loader',
+                    use:[{loader:'css-loader',
+                    options: { minimize: true }}],
+                    
+                })
+                /*use : [
+                    'style-loader',
+                    {   loader:'css-loader',
+                        options:{importLoaders:1}
+                    },
+                    'postcss-loader'
+                ]*/
+            },
+            {
+                test:/\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader:'url-loader',
+                options:{
+                    name:'[hash].[ext]',
+                    limit:10000,
+                }
+            }
+        ]
+    },
+    plugins:[
+        new uglifyJsPlugin(),
+        new HTMLWebpackPlugin({
+            template:path.resolve(__dirname,'index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new OpenBrowserPlugin({url : 'http://localhost:7777'}),
+        new ExtractTextPlugin({
+            filename:'app.css',
+        })
+    ]
+}
+
+module.exports = config;
